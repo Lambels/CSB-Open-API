37accd728ab973b3c31376c6c8194b2802a001aa
Lambels <patrick.arvatu@yahoo.com>
2022-12-27 15:18:05 +0200

    init commit

---
+++ error.go
@@ -0,0 +1,45 @@
+package csb
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	ECONFLICT       = "conflict"
+	EINTERNAL       = "internal"
+	EINVALID        = "invalid"
+	ENOTFOUND       = "not_found"
+	ENOTIMPLEMENTED = "not_implemented"
+	EUNAUTHORIZED   = "unauthorized"
+)
+
+type Error struct {
+	// Machine-readable error code.
+	Code string
+
+	// Human-readable error message.
+	Message string
+}
+
+// Error implements the error interface. Not used by the application otherwise.
+func (e *Error) Error() string {
+	return fmt.Sprintf("csb error: code=%s message=%s", e.Code, e.Message)
+}
+
+func ErrorCode(err error) string {
+	var e *Error
+	if err == nil {
+		return ""
+	} else if errors.As(err, &e) {
+		return e.Code
+	}
+	return EINTERNAL
+}
+
+func Errorf(code string, format string, args ...interface{}) *Error {
+	return &Error{
+		Code:    code,
+		Message: fmt.Sprintf(format, args...),
+	}
+}
