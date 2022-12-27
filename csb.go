9fe138761b8c69ad3fe888d769f0644fb1909a35
Lambels <patrick.arvatu@yahoo.com>
2022-12-27 15:12:18 +0200

    added student and mark types

--- csb.go
+++ csb.go
@@ -1,5 +1,45 @@
 package csb
 
+// Student represents a student in the most current year
+type Student struct {
+	// The pupil ID of the student, this field is required since its the only field which
+	// makes sense for engage.
+	PID int `json:"pid"`
+	// The name of the student.
+	Name string `json:"name"`
+	// CurrentYear represents the current year of the student: "Y11".
+	CurrentYear string `json:"current_year"`
+	// Subjects are all the subjects the student ever took.
+	Subjects []Subject `json:"subjects"`
+
+	// Marks recieved by the student.
+	Marks []*Mark `json:"marks"`
+}
+
+func (s *Student) Validate() error {
+	if s.PID == 0 {
+		return Errorf(EINVALID, "student missing PID field")
+	}
+
+	return nil
+}
+
+// Mark represents the structure of a mark from engage.
+type Mark struct {
+	// Year represents in what year the student was when the mark was recieved.
+	Year string `json:"year"`
+	// At represents the subject at which the mark was recieved.
+	At Subject `json:"at"`
+	// Teacher is the name of the teacher teaching the subject when the mark was recieved.
+	Teacher string `json:"teacher"`
+	// Percentage represents the grade recieved out of 100.
+	Percentage int `json:"percentage"`
+	// Column represents the "significance" of the exam: "Term 2 - Periodic Assessment 1"
+	// or "Term 2 - Final Assessment (F)"
+	Column string `json:"column"`
+}
+
+// Subject is the engage identifier of a subject.
 type Subject string
 
 func (s Subject) String() string {
