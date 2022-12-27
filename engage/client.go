7e6c59dac2188ea0acdb5e1ee0822e2f75e73c05
Lambels <patrick.arvatu@yahoo.com>
2022-12-27 20:57:23 +0200

    init commit

---
+++ engage/client.go
@@ -0,0 +1,171 @@
+package engage
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"github.com/Lambels/go-csb"
+)
+
+var (
+	baseURL               = "https://cambridgeschoolportal.engagehosted.com/Services/ReportCommentServices.asmx/"
+	academicYearsURL      = "GetMarksheetAcademicYears"
+	reportingPeriodsURL   = "GetReportingPeriods"
+	reportingSubjectsURL  = "GetPupilMarksheetSubjects"
+	columnsForSubjectsURL = "GetColumnsForSubjects"
+	marksheetRenderURL    = "RenderPupilMarksheet"
+)
+
+// EngageClient is a client used to interface with the engage api.
+type EngageClient struct {
+	cc *http.Client
+}
+
+// GetAcademicYears gets all the possible academic years for a PID.
+func (c *EngageClient) GetAcademicYears(ctx context.Context, pid string) ([]string, error) {
+	resURL := baseURL + academicYearsURL
+
+	res, err := c.post(ctx, resURL, engageContext{PupilIDs: pid})
+	if err != nil {
+		return nil, err
+	}
+
+	out := make([]string, len(res.D))
+	for _, data := range res.D {
+		out = append(out, data.Value)
+	}
+
+	return out, nil
+}
+
+// GetReportingPeriods gets the reporting periods for a PID in a specific range of academic years.
+func (c *EngageClient) GetReportingPeriods(ctx context.Context, pid string, academicYears []string) ([]string, error) {
+	resURL := baseURL + reportingPeriodsURL
+
+	res, err := c.post(ctx, resURL, engageContext{
+		PupilIDs:      pid,
+		AcademicYears: strings.Join(academicYears, ","),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	out := make([]string, len(res.D))
+	for _, data := range res.D {
+		out = append(out, data.Value)
+	}
+
+	return out, nil
+}
+
+// GetReportingSubjects gets the reporting subjects for a PID in a specific range of academic years and reporting periods.
+func (c *EngageClient) GetReportingSubjects(ctx context.Context, pid string, academicYears, reportingPeriods []string) ([]csb.Subject, error) {
+	resURL := baseURL + reportingSubjectsURL
+
+	res, err := c.post(ctx, resURL, engageContext{
+		PupilIDs:         pid,
+		AcademicYears:    strings.Join(academicYears, ","),
+		ReportingPeriods: strings.Join(reportingPeriods, ","),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	out := make([]csb.Subject, len(res.D))
+	for _, data := range res.D {
+		out = append(out, csb.Subject(data.Value))
+	}
+
+	return out, nil
+}
+
+// GetColumnsForSubjects gets the "columns" for a pid in a specified academic years and periods range for the specified subjects.
+// A column refers to the type of exam.
+func (c *EngageClient) GetColumnsForSubjects(ctx context.Context, pid string, academicYears, reportingPeriods []string, subjects []csb.Subject) ([]string, error) {
+	resURL := baseURL + reportingPeriodsURL
+
+	res, err := c.post(ctx, resURL, engageContext{
+		PupilIDs:         pid,
+		AcademicYears:    strings.Join(academicYears, ","),
+		ReportingPeriods: strings.Join(reportingPeriods, ","),
+		SubjectList:      csb.Concat(subjects),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	out := make([]string, len(res.D))
+	for _, data := range res.D {
+		out = append(out, data.Value)
+	}
+
+	return out, nil
+}
+
+func (c *EngageClient) GetMarksheetRender(ctx context.Context, pid string, academicYears, subjectColumns, reportingPeriods []string, reportingSubjects []csb.Subject) (string, error) {
+
+}
+
+// post sends a post request to url with the specified engage context. It checks for any errors during
+// the exchange process with engage. It returns an engage response which has
+// at least one piece of data inside.
+func (c *EngageClient) post(ctx context.Context, url string, engCtx engageContext) (res *engageResponse, err error) {
+	body, err := json.Marshal(engCtx)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(body))
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := c.cc.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, decodeError(resp)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
+		return nil, err
+	}
+
+	// parse response here for status code not found since engage is very wierd with response
+	// codes. (an invalid pid results in StatusCodeOK)
+	if len(res.D) == 0 {
+		return nil, csb.Errorf(csb.ENOTFOUND, "engage: invalid PID: %v", engCtx.PupilIDs)
+	}
+
+	return res, nil
+}
+
+// NewEngageClient creates a new engage client with the provided token used for
+// authentification.
+func NewEngageClient(c *http.Client, token string) *EngageClient {
+	c.Transport = &cookieHeaderTransport{
+		cookie: token,
+		d:      c.Transport,
+	}
+
+	return &EngageClient{
+		cc: c,
+	}
+}
+
+type cookieHeaderTransport struct {
+	cookie string
+	d      http.RoundTripper
+}
+
+func (t *cookieHeaderTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Add("Cookie", t.cookie)
+	return t.d.RoundTrip(r)
+}
