6a5ceb118de0559fbf480eabe9c6bd71b4f10394
Lambels <patrick.arvatu@yahoo.com>
2022-12-29 23:02:59 +0200

    added fields + docs

--- config.go
+++ config.go
@@ -1,21 +1,34 @@
 package csb
 
+// Config represents the structure of a json config file.
 type Config struct {
-	HTTP   httpConfig   `json:"http"`
-	Sqlite sqliteConfig `json:"sqlite"`
-	Engage engageConfig `json:"engage"`
+	HTTP   httpConfig   `json:"http"`   // HTTP related configs.
+	Sqlite sqliteConfig `json:"sqlite"` // Sqlite related configs.
+	Engage engageConfig `json:"engage"` // Engage related configs.
 }
 
+// engageConfig holds all the config fields related to engage.
 type engageConfig struct {
-	Token   string `json:"token"`
-	SaveNew bool   `json:"save_new"`
+	// Token used for engage auth.
+	Token string `json:"token"`
+	// Fallback indicates wether failed queries to the database should fallback to engage.
+	Fallback bool `json:"fallback"`
 }
 
+// httpConfig holds all the config fields related to http services.
 type httpConfig struct {
-	AddrBackend  string `json:"addr_backend"`
+	// AddrBackend is the http adress of the backend server.
+	AddrBackend string `json:"addr_backend"`
+	// AddrFrontend is the http adress of the frontend server.
 	AddrFrontend string `json:"addr_frontend"`
 }
 
+// sqliteConfig holds all the config fields related to the sqlite database.
 type sqliteConfig struct {
+	// DSN is the data source name of the database.
+	//
+	// :memory: isnt allowed.
 	DSN string `json:"dsn"`
+	// MigrationsPath is the path to the migrations folder.
+	MigrationsPath string `json:"migrations_path"`
 }
